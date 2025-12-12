4ba226f4797f7e95c4ccb831fc6163fd3f59ef83
yoshbatula <yoshbatula2@gmail.com>
2025-12-12 21:45:17 +0800

    I don't know what to do

--- src/login.php
+++ src/login.php
@@ -7,7 +7,14 @@
     <link rel="stylesheet" href="css/output.css">
     <link rel="icon" href="../images/CCE-LOGO.svg" type="image/svg+xml">
 </head>
-<body class="bg-black">
-    <h1 class="text-white text-center items-center">HELLO WORLD!</h1>
+<body>
+    <!-- Background Image -->
+    <div class="fixed inset-0 -z-10 w-10 h-10">
+        <img src="../images/CCE-BACKGROUND.png" alt="Background Image" class="w-30 h-10 object-cover">
+    </div>
+    <div class="flex flex-row items-center justify-center text-center h-screen">
+        <!-- Content goes here -->
+    </div>
+    <h1 class="text-center font-bold">Hi Yosh</h1>
 </body>
 </html>
