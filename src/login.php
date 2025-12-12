9343cf72f673279b69797e74f04c2c53d18f0dbd
yoshbatula <yoshbatula2@gmail.com>
2025-12-12 21:25:01 +0800

    just adding new features:

--- src/login.php
+++ src/login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN FORM</title>
     <link rel="stylesheet" href="css/output.css">
-    <link rel="icon" href="">
+    <link rel="icon" href="../images/CCE-LOGO.svg" type="image/svg+xml">
 </head>
 <body class="bg-black">
     <h1 class="text-white text-center items-center">HELLO WORLD!</h1>
